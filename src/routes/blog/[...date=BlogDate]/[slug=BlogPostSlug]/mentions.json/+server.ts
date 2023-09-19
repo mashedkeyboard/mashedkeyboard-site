f40ec324211ae3210d9e412ed1b756a6d028272b
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2023-09-19 22:50:19 +0100

    Add logging for debug

--- src/routes/blog/[...date=BlogDate]/[slug=BlogPostSlug]/mentions.json/+server.ts
+++ src/routes/blog/[...date=BlogDate]/[slug=BlogPostSlug]/mentions.json/+server.ts
@@ -8,52 +8,55 @@ export const prerender = false;
 
 /** @type {import('./$types').PageServerLoad} */
 export async function GET({ request, params, platform }) {
-    const resolvedSlug = resolveSlug(params);
-    const normalisedUrl = `https://${PUBLIC_HOSTNAME}/blog/${resolvedSlug}/mentions.json`;
-    let allMentions = [];
+  const resolvedSlug = resolveSlug(params);
+  const normalisedUrl = `https://${PUBLIC_HOSTNAME}/blog/${resolvedSlug}/mentions.json`;
+  let allMentions = [];
 
-    const kvStore = platform?.env?.BLOG_WEBMENTIONS;
-    const cache = platform?.caches?.default;
+  const kvStore = platform?.env?.BLOG_WEBMENTIONS;
+  const cache = platform?.caches?.default;
 
-    if (kvStore && cache) {
-        const response = await cache.match(normalisedUrl);
-        if (response) return response;
+  if (kvStore && cache) {
+    console.log(normalisedUrl);
+    const response = await cache.match(normalisedUrl);
+    console.log(response || "No response in cache");
+    if (response) return response;
 
-        let mentionKeys: {keys: [{name: string}], list_complete: boolean, cursor: string} | undefined;
-        do {
-            mentionKeys = await kvStore.list({ prefix: resolvedSlug + '/mentions', cursor: mentionKeys?.cursor });
-            for (let key of mentionKeys?.keys || []) {
-                allMentions.push(await kvStore.get(key.name, {type: "json"}) as Webmention);
-            }
-        } while (mentionKeys?.list_complete == false);
-    } else {
-        throw error(500, 'no KV store available');
-    }
+    let mentionKeys: { keys: [{ name: string }], list_complete: boolean, cursor: string } | undefined;
+    do {
+      mentionKeys = await kvStore.list({ prefix: resolvedSlug + '/mentions', cursor: mentionKeys?.cursor });
+      for (let key of mentionKeys?.keys || []) {
+        allMentions.push(await kvStore.get(key.name, { type: "json" }) as Webmention);
+      }
+    } while (mentionKeys?.list_complete == false);
+  } else {
+    throw error(500, 'no KV store available');
+  }
 
-    let mentionsSet = new Set<Webmention>(allMentions);
-    let mentionsUrls: {[url: string]: Webmention} = Object.fromEntries(allMentions.map((mention) => [mention.mfItem?.properties?.url || mention.url, mention]));
+  let mentionsSet = new Set<Webmention>(allMentions);
+  let mentionsUrls: { [url: string]: Webmention } = Object.fromEntries(allMentions.map((mention) => [mention.mfItem?.properties?.url || mention.url, mention]));
 
-    mentionsSet.forEach((mention) => {
-      const inReplyTo = mention.mfItem?.properties?.["in-reply-to"];
-      if (inReplyTo) {
-        for (let replyUrl of inReplyTo) {
-          if (mentionsUrls[replyUrl.toString()]) {
-            const replyToMention = mentionsUrls[replyUrl.toString()];
-            replyToMention.replies ||= [];
-            replyToMention.replies.push(mention);
-            mentionsSet.delete(mention);
-          }
+  mentionsSet.forEach((mention) => {
+    const inReplyTo = mention.mfItem?.properties?.["in-reply-to"];
+    if (inReplyTo) {
+      for (let replyUrl of inReplyTo) {
+        if (mentionsUrls[replyUrl.toString()]) {
+          const replyToMention = mentionsUrls[replyUrl.toString()];
+          replyToMention.replies ||= [];
+          replyToMention.replies.push(mention);
+          mentionsSet.delete(mention);
         }
       }
-    })
-
-    const resp = json(Array.from(mentionsSet).sort((m1, m2) => m2.date > m1.date ? 1 : m2.date == m1.date ? 0 : -1));
-
-    if (cache) {
-      const cacheResp = resp.clone();
-      cacheResp.headers.append("Cache-Control", "public, s-maxage=604800");
-      await cache.put(normalisedUrl, cacheResp);
     }
+  })
 
-    return resp;
+  const resp = json(Array.from(mentionsSet).sort((m1, m2) => m2.date > m1.date ? 1 : m2.date == m1.date ? 0 : -1));
+
+  if (cache) {
+    const cacheResp = resp.clone();
+    cacheResp.headers.append("Cache-Control", "public, s-maxage=604800");
+    console.log(cacheResp);
+    await cache.put(normalisedUrl, cacheResp);
   }
+
+  return resp;
+}
