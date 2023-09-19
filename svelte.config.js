eca38d2d1c337b6e51f9be7fbaa7f1408d5cb9fc
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2023-09-19 22:30:43 +0100

    Expect 405 on Webmention endpoint

--- svelte.config.js
+++ svelte.config.js
@@ -16,6 +16,17 @@ const config = {
 		inlineStyleThreshold: 4096, // inline styles smaller than 4kb
 		csrf: {
 			checkOrigin: false // this is normally very sensible behaviour, but the only route we use here is webmention, and we don't want it for it.
+		},
+		prerender: {
+			handleHttpError: ({ path, status, message }) => {
+				if (path === '/blog/webmention' && status == 405) {
+					// expect 405 Method Not Allowed on GET request to webmention endpoint
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
 		}
 	},
 	extensions: [
