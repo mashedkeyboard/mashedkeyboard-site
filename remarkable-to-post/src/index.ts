e215fdcdf567504c7e4b3da738121650c58872de
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2023-10-13 10:30:31 +0100

    create-post-from-remarkable first pass

--- remarkable-to-post/src/index.ts
+++ remarkable-to-post/src/index.ts
@@ -1,32 +1,78 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import TurndownService from 'turndown';
+import { Octokit } from "@octokit/core";
+import { simpleParser } from 'mailparser';
+import { Readable } from 'node:stream';
+import type { ReadableStream } from 'node:stream/web';
+import { lightFormat } from 'date-fns';
+import slugify from 'slugify';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	RECV_KEY: string;
+	GITHUB_AUTH_TOKEN: string;
+	BRANCH: string;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+	async email(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext) {
+		if (!message.to.includes(env.RECV_KEY) || message.from != 'my@remarkable.com') {
+			message.setReject("Unknown address");
+			return;
+		}
+
+		simpleParser(Readable.fromWeb(message.raw as ReadableStream)).then(async (parsed) => {
+			if (!parsed.html) {
+				message.setReject("No HTML");
+				return;
+			}
+
+			// Don't escape markdown: I want to write in it.
+			TurndownService.prototype.escape = (x) => x;
+
+			const turndownService = new TurndownService();
+
+			const [headers, mainBodyHtml] = parsed.html.split(/<\s*hr\s*\/?>/, 2);
+
+			const mainMarkdown = turndownService.turndown(mainBodyHtml.split(/<\s*\/body\s*>/, 1)[0]);
+
+			const parsedHeaders: {
+				title: string,
+				date?: string,
+				slug?: string,
+				image?: string,
+				imageAlt?: string
+			} = Object.fromEntries(headers.split(/<\s*body\s*>/, 2)[1].split(/(?:<\s*br\s*\/?>|\n)+/).flatMap((item) => {
+				const returnedItems = [];
+				
+				item = item.trim();
+				if (item.length) returnedItems.push(item.split(":").map((i) => i.trim()));
+
+				return returnedItems;
+			}))
+
+			const octokit = new Octokit({
+				auth: env.GITHUB_AUTH_TOKEN
+			})
+
+			const postDate = parsedHeaders.date ? new Date(parsedHeaders.date) : Date.now()
+			const postSlug = parsedHeaders.slug || slugify(parsedHeaders.title)
+
+			const markdown = `---
+${Object.entries(parsedHeaders).map(([k, v]) => `${k}: ${v}`).join("\n")}
+---
+${mainMarkdown}`
+
+			await octokit.request('POST /repos/mashedkeyboard/mashedkeyboard-site/' + 
+								'actions/workflows/create-post-from-remark-email/dispatches', {
+				ref: env.BRANCH,
+				inputs: {
+					path: `${lightFormat(postDate, 'yyyy/MM/dd')}/${postSlug}.svx`,
+					title: parsedHeaders.title,
+					markdown: markdown
+				},
+				headers: {
+					'X-GitHub-Api-Version': '2022-11-28'
+				}
+			})
+		});
+	}
 };
