dcb1e3c433d500ceb6384949a746471ed3c806d6
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2023-10-13 11:50:30 +0100

    prettier run

--- remarkable-to-post/src/index.ts
+++ remarkable-to-post/src/index.ts
@@ -1,5 +1,5 @@
 import TurndownService from 'turndown';
-import { Octokit } from "@octokit/core";
+import { Octokit } from '@octokit/core';
 import { readEml } from 'eml-parse-js';
 import { lightFormat } from 'date-fns';
 import slugify from 'slugify';
@@ -13,22 +13,22 @@ export interface Env {
 export default {
 	async email(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext) {
 		if (!message.to.includes(env.RECV_KEY) || !message.from.endsWith('@my.remarkable.com')) {
-			console.error("Unknown address, rejecting");
-			message.setReject("Unknown address");
+			console.error('Unknown address, rejecting');
+			message.setReject('Unknown address');
 			return;
 		}
 
 		readEml(await new Response(message.raw).text(), async (err, emlJson) => {
 			if (err) {
-				console.error("Errored while parsing email: ", err);
-				message.setReject("Failed to process due to email parsing error");
+				console.error('Errored while parsing email: ', err);
+				message.setReject('Failed to process due to email parsing error');
 				return;
 			}
 
 			const html = emlJson?.html;
 			if (!html) {
-				console.error("No parseable HTML detected, rejecting");
-				message.setReject("No HTML");
+				console.error('No parseable HTML detected, rejecting');
+				message.setReject('No HTML');
 				return;
 			}
 
@@ -37,66 +37,77 @@ export default {
 
 			const turndownService = new TurndownService();
 
-			console.log("Initialised Turndown");
+			console.log('Initialised Turndown');
 
 			const [headers, mainBodyHtml] = html.split(/<\s*hr\s*\/?>/, 2);
 
-			console.log("Detected headers: ", headers);
+			console.log('Detected headers: ', headers);
 
-			const h1 = new DOMParser().parseFromString(mainBodyHtml, "text/html").querySelector("h1")?.innerHTML;
+			const h1 = new DOMParser().parseFromString(mainBodyHtml, 'text/html').querySelector('h1')?.innerHTML;
 
 			turndownService.remove('h1');
 
 			const mainMarkdown = turndownService.turndown(mainBodyHtml.split(/<\s*\/body\s*>/, 1)[0]);
 
 			const parsedHeaders: {
-				title?: string,
-				date?: string,
-				slug?: string,
-				image?: string,
-				imageAlt?: string
-			} = Object.fromEntries(headers.split(/<\s*body\s*>/, 2)[1].split(/(?:<\s*br\s*\/?>|\n)+/).flatMap((item) => {
-				const returnedItems = [];
-				
-				item = item.trim();
-				if (item.length) returnedItems.push(item.split(":").map((i) => i.trim()));
+				title?: string;
+				date?: string;
+				slug?: string;
+				image?: string;
+				imageAlt?: string;
+			} = Object.fromEntries(
+				headers
+					.split(/<\s*body\s*>/, 2)[1]
+					.split(/(?:<\s*br\s*\/?>|\n)+/)
+					.flatMap((item) => {
+						const returnedItems = [];
 
-				return returnedItems;
-			}))
+						item = item.trim();
+						if (item.length) returnedItems.push(item.split(':').map((i) => i.trim()));
 
-			const postTitle = parsedHeaders.title || h1 || "Untitled";
+						return returnedItems;
+					}),
+			);
+
+			const postTitle = parsedHeaders.title || h1 || 'Untitled';
 
 			const octokit = new Octokit({
-				auth: env.GITHUB_AUTH_TOKEN
-			})
-
-			const postDate = parsedHeaders.date ? new Date(parsedHeaders.date) : new Date()
-			const postSlug = parsedHeaders.slug || slugify(postTitle)
-
-			const markdown = `---
-	title: ${postTitle}
-	date: ${postDate.toISOString()}
-	${parsedHeaders.image ? `image: ${parsedHeaders.image}
-	imageAlt: ${parsedHeaders.imageAlt}` : ''}
-	---
-	${mainMarkdown}`
-
-			console.log("Making request to GitHub");
-
-			const resp = await octokit.request('POST /repos/mashedkeyboard/mashedkeyboard-site/' + 
-								'actions/workflows/create-post-from-remark-email/dispatches', {
-				ref: env.BRANCH,
-				inputs: {
-					path: `${lightFormat(postDate, 'yyyy/MM/dd')}/${postSlug}.svx`,
-					title: postTitle,
-					markdown: markdown
-				},
-				headers: {
-					'X-GitHub-Api-Version': '2022-11-28'
-				}
+				auth: env.GITHUB_AUTH_TOKEN,
 			});
 
-			console.log("GitHub responded with ", resp.status, ": ", resp.data)
+			const postDate = parsedHeaders.date ? new Date(parsedHeaders.date) : new Date();
+			const postSlug = parsedHeaders.slug || slugify(postTitle);
+
+			const markdown = `---
+title: ${postTitle}
+date: ${postDate.toISOString()}
+${
+	parsedHeaders.image
+		? `image: ${parsedHeaders.image}
+imageAlt: ${parsedHeaders.imageAlt}`
+		: ''
+}
+---
+${mainMarkdown}`;
+
+			console.log('Making request to GitHub');
+
+			const resp = await octokit.request(
+				'POST /repos/mashedkeyboard/mashedkeyboard-site/' + 'actions/workflows/create-post-from-remark-email/dispatches',
+				{
+					ref: env.BRANCH,
+					inputs: {
+						path: `${lightFormat(postDate, 'yyyy/MM/dd')}/${postSlug}.svx`,
+						title: postTitle,
+						markdown: markdown,
+					},
+					headers: {
+						'X-GitHub-Api-Version': '2022-11-28',
+					},
+				},
+			);
+
+			console.log('GitHub responded with ', resp.status, ': ', resp.data);
 		});
-	}
+	},
 };
