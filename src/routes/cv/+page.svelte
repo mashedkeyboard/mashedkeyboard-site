d595487d03b5c3d0a630a2724fed735cd1aa154d
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2023-10-22 12:20:18 +0100

    CV update

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -24,15 +24,16 @@
 
 	<p>
 		I'm a candidate who offers the advantages of someone with experience in the industry alongside a
-        <strong>fresh and highly motivated mind</strong>, with a keen and curious interest in technology and digital regulation.
+		<strong>fresh and highly motivated mind</strong>, with a keen and curious interest in technology
+		and digital regulation.
 	</p>
 
 	<p>
 		<strong>I passionately believe in the power of humans combined with software to change the world for
-			good</strong>, and see myself as someone who can help lead that change. Having
+		good</strong>, and see myself as someone who can help lead that change. Having
 		<strong>founded my own company</strong>
-		just two months after turning 17 to help campaigns I believed in realise their goals, I've got
-		clear experience working in a <strong>self-motivated</strong> fashion to achieve key objectives.
+		just two months after turning 17 to help campaigns I believed in realise their goals, I've got clear
+		experience working in a <strong>self-motivated</strong> fashion to achieve key objectives.
 		Unlike many other candidates, I bring this experience on day one -
 		<strong>soft skills along with the technical experience</strong> coming with it - meaning a faster
 		onboarding process and a greater value to your team.
@@ -45,23 +46,25 @@
 		<details>
 			<summary
 				><FaIcon icon={faLaptopCode} /> <strong>Full Stack Developer</strong> at Poteris: July 2023 &mdash;
-				present</summary
-			>
+				present</summary>
 			<p>
-				Loudspeek provides digital advocacy solutions to some of the UK's largest progressive change
-				campaigns and charities. I am the founder and director, and wrote the codebase myself, which
-				processes nearly 2 million user requests per month at peak. I have experience from this role
-				developing with Ruby on Rails, PostgreSQL and Redis, among various other technologies. In
-				terms of soft skills, I also overcame significant adversity in the process of founding
-				Loudspeek because of my age, including not being able to open a bank account for the company
-				for more than six months until I finally persuaded a bank to do so.
+				Poteris is a non-profit agency working with organisations to create positive change through
+				technology, in particular in the third sector and civic technology spaces. As part a core
+				team of three, I'm the only in-house software engineer, with responsibilities including
+				building and maintaining systems that manage campaign data, architecting data models for the
+				storage of authentication and contact information, managing the deployment of internal and
+				external systems into the AWS cloud, and writing internal documentation for contracted
+				technology partners.
+			</p>
+			<p>
+				In this role, I've worked in both TypeScript and Golang, using frameworks like Nest.js and
+				Next.js, as well as performing DevOps tasks across both AWS and Vercel platforms.
 			</p>
 		</details>
 		<details>
 			<summary
 				><FaIcon icon={faAddressCard} /> <strong>Director</strong> at Loudspeek: February 2019 &mdash;
-				present</summary
-			>
+				present</summary>
 			<p>
 				Loudspeek provides digital advocacy solutions to some of the UK's largest progressive change
 				campaigns and charities. I am the founder and director, and wrote the codebase myself, which
@@ -75,8 +78,7 @@
 		<details>
 			<summary
 				><FaIcon icon={faLaptopCode} /> <strong>Computer Scientist</strong> at Netcraft: July to September
-				2022</summary
-			>
+				2022</summary>
 			<p>
 				I spent the summer between my second and third years at university working at Netcraft's
 				London office as a Computer Scientist. In this role I designed and developed automated
@@ -88,8 +90,7 @@
 		<details>
 			<summary
 				><FaIcon icon={faScaleBalanced} /> <strong>SPARK Scheme Participant</strong> at Clifford Chance:
-				July 2021</summary
-			>
+				July 2021</summary>
 			<p>
 				In my first year at university, this undergraduate placement scheme gave me an opportunity
 				to experience a Magic Circle law firm. Having won my place on the scheme through my being
@@ -106,48 +107,57 @@
 		<details>
 			<summary
 				><FaIcon icon={faAmbulance} />
-				<strong>Emergency Ambulance Crew</strong> at St John Ambulance: October 2022
-				&mdash; present</summary
-			>
+				<strong>Emergency Ambulance Crew</strong> at St John Ambulance: October 2022 &mdash; present</summary>
 			<p>
-				St John Ambulance is now the UK's national Ambulance Auxiliary, meaning we provide support to NHS ambulance services across the UK all year round. I qualified as an Emergency Ambulance Crew with St John in October 2022, meaning I attend medical emergencies on behalf of the NHS 999 and 111 services.St John Ambulance is now the UK's national Ambulance Auxiliary, meaning we provide support to NHS ambulance services across the UK all year round.
+				St John Ambulance is now the UK's national Ambulance Auxiliary, meaning we provide support
+				to NHS ambulance services across the UK all year round. I qualified as an Emergency
+				Ambulance Crew with St John in October 2022, meaning I attend medical emergencies on behalf
+				of the NHS 999 and 111 services.St John Ambulance is now the UK's national Ambulance
+				Auxiliary, meaning we provide support to NHS ambulance services across the UK all year
+				round.
 			</p>
 			<p>
-				I qualified as an Emergency Ambulance Crew with St John in October 2022, meaning I attend medical emergencies on behalf of the NHS 999 and 111 services.
+				I qualified as an Emergency Ambulance Crew with St John in October 2022, meaning I attend
+				medical emergencies on behalf of the NHS 999 and 111 services.
 			</p>
 		</details>
 		<details>
 			<summary
 				><FaIcon icon={faHandsHoldingCircle} /> <strong>Trustee</strong> at the University of Southampton
-				Students' Union: July 2022 &mdash; July 2023</summary
-			>
+				Students' Union: July 2022 &mdash; July 2023</summary>
 			<p>
-				I served as a Student Trustee of SUSU - the University of Southampton Students'
-				Union - in my final year of study at the University of Southampton. In this role I was responsible for ensuring the Union is operating in accordance with
-				its policies, its objects and the law.
+				I served as a Student Trustee of SUSU - the University of Southampton Students' Union - in
+				my final year of study at the University of Southampton. In this role I was responsible for
+				ensuring the Union is operating in accordance with its policies, its objects and the law.
 			</p>
 			<p>
-				I worked with a Board composed of members with a wide range of multidisciplinary backgrounds, and oversaw the beginning of a broad-ranging governance transformation project. I sat on the Union's Audit and Risk Subcommittee of the Board, working alongside external auditors examining risks including financial, reputational, and safeguarding risks.
+				I worked with a Board composed of members with a wide range of multidisciplinary
+				backgrounds, and oversaw the beginning of a broad-ranging governance transformation project.
+				I sat on the Union's Audit and Risk Subcommittee of the Board, working alongside external
+				auditors examining risks including financial, reputational, and safeguarding risks.
 			</p>
 		</details>
 		<details>
 			<summary
 				><FaIcon icon={faCalendarCheck} />
-				<strong>Unit Events Lead</strong> at St John Ambulance University of Southampton: June 2021
-				&mdash; May 2023</summary
-			>
+				<strong>Unit Events Lead</strong> at St John Ambulance University of Southampton: June 2021 &mdash;
+				May 2023</summary>
 			<p>
-				St John Ambulance is the UK's auxiliary ambulance service, its leading first aid charity, and its national health volunteer reserve. Student Volunteering Units in St John act as regular Event Services Units, and are trained and run to the same standard as any other adult unit.
+				St John Ambulance is the UK's auxiliary ambulance service, its leading first aid charity,
+				and its national health volunteer reserve. Student Volunteering Units in St John act as
+				regular Event Services Units, and are trained and run to the same standard as any other
+				adult unit.
 			</p>
 			<p>
-				As Unit Events Lead of the University of Southampton unit, my role was to manage the deployment to events of students at the university giving up their time to volunteer with St John, and ensuring we appropriately staffed our events.
+				As Unit Events Lead of the University of Southampton unit, my role was to manage the
+				deployment to events of students at the university giving up their time to volunteer with St
+				John, and ensuring we appropriately staffed our events.
 			</p>
 		</details>
 		<details>
 			<summary
 				><FaIcon icon={faMicrochip} /> <strong>Technical Officer</strong> at the University of Southampton
-				LGBTQ+ Society: November 2020 &mdash; July 2022</summary
-			>
+				LGBTQ+ Society: November 2020 &mdash; July 2022</summary>
 			<p>
 				The UoS LGBTQ+ Society is the Students' Union society for students from gender and sexual
 				minorities (GSMs). In my role as Technical Officer, I was responsible not only for digital
@@ -168,8 +178,7 @@
 				<span class="fa-li"><FaIcon icon={faTrophy} /></span>
 				Clifford Chance Young Activist of the Year in the
 				<a href="https://undergraduateoftheyear.com/winners/2021" target="_blank" rel="noreferrer"
-					>TARGETjobs Undergraduate of the Year Awards 2021</a
-				>
+					>TARGETjobs Undergraduate of the Year Awards 2021</a>
 			</li>
 			<li>
 				<span class="fa-li"><FaIcon icon={faNewspaper} /></span>
@@ -177,8 +186,8 @@
 				<a
 					href="https://www.theguardian.com/education/2020/aug/16/a-level-student-launches-legal-bid-against-ofqual"
 					target="_blank"
-					rel="noreferrer">various UK and international media</a
-				> in relation to legal action pursued against Ofqual in 2020
+					rel="noreferrer">various UK and international media</a> in relation to legal action pursued
+				against Ofqual in 2020
 			</li>
 			<li>
 				<span class="fa-li"><FaIcon icon={faPenNib} /></span>
@@ -186,8 +195,7 @@
 				<a
 					href="https://www.orwellfoundation.com/the-orwell-youth-prize/2018-youth-prize/previous-winners-youth/2016-winners/"
 					target="_blank"
-					rel="noreferrer">Orwell Youth Prize 2016</a
-				>
+					rel="noreferrer">Orwell Youth Prize 2016</a>
 			</li>
 		</ul>
 	</section>
@@ -198,7 +206,6 @@
 			<section class="flex-30">
 				<h4>University of Southampton</h4>
 				<section>
-					<h5>2020 &mdash; 2023</h5>
 					<p>BSc Computer Science</p>
 					<p>First Class Honours</p>
 					<aside>
@@ -216,7 +223,6 @@
 			<section class="flex-50">
 				<h4>Elthorne Park High School</h4>
 				<section>
-					<h5>2018 &mdash; 2020</h5>
 					<p>Extended Project Qualification &mdash; A*</p>
 					<p>A levels:</p>
 					<ul>
@@ -227,7 +233,6 @@
 					</ul>
 				</section>
 				<section>
-					<h5>2016 &mdash; 2018</h5>
 					<p>GCSEs:</p>
 					<ul>
 						<li>Computer Science &mdash; 9</li>
@@ -247,18 +252,15 @@
 			<section class="flex-30">
 				<h4>St John Ambulance</h4>
 				<section>
-					<h5>2022</h5>
 					<p>Emergency Ambulance Crew</p>
 				</section>
 				<section>
-					<h5>2021</h5>
 					<p>Event Manager</p>
 				</section>
 			</section>
 			<section class="flex-30">
 				<h4>NationBuilder</h4>
 				<section>
-					<h5>2019</h5>
 					<p>Certified Developer</p>
 				</section>
 			</section>
