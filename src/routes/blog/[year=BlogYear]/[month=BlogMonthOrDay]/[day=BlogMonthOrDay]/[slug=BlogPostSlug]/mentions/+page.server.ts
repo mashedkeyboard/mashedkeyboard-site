0883b79b277827e1f3785d925cf45a0958e3d172
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2025-07-08 22:34:50 +0100

    Make mentions JSON request internally

--- src/routes/blog/[year=BlogYear]/[month=BlogMonthOrDay]/[day=BlogMonthOrDay]/[slug=BlogPostSlug]/mentions/+page.server.ts
+++ src/routes/blog/[year=BlogYear]/[month=BlogMonthOrDay]/[day=BlogMonthOrDay]/[slug=BlogPostSlug]/mentions/+page.server.ts
@@ -1,16 +1,17 @@
 import { resolveSlug } from '$lib/blog/SlugResolver';
 import { getPost } from '$lib/blog/PostManager';
 import { urlForPost } from '$lib/Helpers';
+import { GET as getMentionsJson } from '../mentions.json/+server.js';
 
 export const prerender = false;
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ params, fetch }) {
+export async function load({ params, platform }) {
 	const post = await getPost(resolveSlug(params));
 	return {
 		meta_title: `Mentions: ${post.getTitle()}`,
 		has_own_h1: true,
-		mentions: await fetch(`${urlForPost(post)}/mentions.json`).then((resp) => resp.json()),
+		mentions: (await getMentionsJson({ params, platform })).json(),
 		postName: post.getTitle(),
 		postUrl: urlForPost(post),
 		postMastoUrl: post.getMastodonPost(),
